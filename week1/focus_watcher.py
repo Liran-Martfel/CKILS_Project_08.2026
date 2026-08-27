c3efc9d006643be9482b39c73c443c4f0605a3dd
Liran Martfel <liran.martfel@gmail.com>
2026-08-27 11:00:12 +0300

    version 1.3 - swiching automaticly to english when program runs

--- week1/focus_watcher.py
+++ week1/focus_watcher.py
@@ -1,6 +1,24 @@
 import ctypes
 import ctypes.wintypes
+import win32api
 import win32con, win32gui, win32process, psutil
+
+
+##identify the numeric language ID in 16 bits - need to see: 40d for hebrew and 409 for english
+layouts = win32api.GetKeyboardLayoutList()
+for hkl in layouts:
+    lcid = hkl & 0xFFFF  # the low 16 bits are the language ID
+    print(f"HKL: {hex(hkl)}   LCID: {hex(lcid)}")
+
+#HKL = handle to a keyboard layout
+English_HKL = 0x4090409
+Hebrew_HKL = 0x040d040d
+
+hwnd = win32gui.GetForegroundWindow()
+# This is the same message Windows sends itself when switching
+# layout by hand with Alt+Shift — we're just sending it programmatically.
+win32api.PostMessage(hwnd, win32con.WM_INPUTLANGCHANGEREQUEST, 0, English_HKL)
+##############################################################################################
 
 user32 = ctypes.windll.user32  ## direct access to Windows' user32.dll — this is where
 ## SetWinEventHook actually lives, since pywin32 doesn't wrap it
@@ -45,3 +63,4 @@ hook = user32.SetWinEventHook(
 
 print("Watching for focus changes. Alt-Tab between a few windows. Ctrl+C to stop.")
 win32gui.PumpMessages()  ## the message loop — keeps the program alive and listening
+
